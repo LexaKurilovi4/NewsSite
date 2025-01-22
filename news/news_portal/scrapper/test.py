a14616159df5d0529161e3b1f5e0b1a28b4a35b6
LexaKurilovich <lexakurilovi4@gmail.com>
2025-01-22 20:19:14 +0300

    add some features

--- news/news_portal/scrapper/test.py
+++ news/news_portal/scrapper/test.py
@@ -18,6 +18,17 @@ news_link = "/".join(news_link.split("/")[3::])
 html = requests.get(host + news_link)
 soup = BeautifulSoup(html.text, "html.parser")
 title = soup.find("h1", class_="tm-title tm-title_h1").span.get_text()
-datetime_string = soup.find("span", class_="tm-article-datetime-published").time["title"]
+body = soup.find("div", class_="article-formatted-body article-formatted-body article-formatted-body_version-2").div
+news_text = ""
+news_pictures = ""
+author = soup.find("a", class_="tm-user-info__username").text
+for tag in body:
+    if tag.name == "p":
+        news_text += tag.text + "\n"
+    if tag.name == "figure":
+        news_pictures += tag.img["src"] + "\n"
+        news_text += "img\n"
 
-print(soup.find("div", class_="article-formatted-body article-formatted-body article-formatted-body_version-2"))
+print(soup.find("a", class_="tm-user-info__username").text)
+
+#print(body)

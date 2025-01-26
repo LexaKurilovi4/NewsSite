04985136cd77210b159a669f449941b6fd5e0635
LexaKurilovich <lexakurilovi4@gmail.com>
2025-01-26 14:22:52 +0300

    add some features

--- news/news_portal/scrapper/test.py
+++ news/news_portal/scrapper/test.py
@@ -13,22 +13,27 @@ def get_news_links():
         links.append(item.find("a", "tm-title__link")["href"])
     return links
 
-news_link = get_news_links()[0]
+news_link = get_news_links()[1]
 news_link = "/".join(news_link.split("/")[3::])
 html = requests.get(host + news_link)
 soup = BeautifulSoup(html.text, "html.parser")
 title = soup.find("h1", class_="tm-title tm-title_h1").span.get_text()
 body = soup.find("div", class_="article-formatted-body article-formatted-body article-formatted-body_version-2").div
 news_text = ""
-news_pictures = ""
+news_pictures = dict()
 author = soup.find("a", class_="tm-user-info__username").text
+i = 1
 for tag in body:
     if tag.name == "p":
         news_text += tag.text + "\n"
     if tag.name == "figure":
-        news_pictures += tag.img["src"] + "\n"
-        news_text += "img\n"
+        news_pictures.update({str(i): {"url": tag.img["src"]}})
+        news_text += "{" + str(i) + "}"
+    if tag.name == "div":
+        news_pictures.update({f"v{i}": {"url": tag["data-src"]}})
+    i += 1
+    print(tag)
 
-print(soup.find("a", class_="tm-user-info__username").text)
+#print(soup.find("a", class_="tm-user-info__username").text)
 
-#print(body)
+print(news_text)

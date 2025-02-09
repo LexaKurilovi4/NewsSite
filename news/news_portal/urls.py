6504282a44a657e713bda5cfecf92fb5bc54afb2
LexaKurilovich <lexakurilovi4@gmail.com>
2025-02-09 14:11:44 +0300

    add url patter for scrapper

--- news/news_portal/urls.py
+++ news/news_portal/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:pk>/', views.NewsDetailView.as_view(), name='news_view'),
     path('tags/', views.get_tags, name='tags'),
     path('search/<query>', views.SearchListView.as_view(), name='search'),
-    path('category/<int:pk>', views.CategoryListView.as_view(), name='category')
+    path('category/<int:pk>', views.CategoryListView.as_view(), name='category'),
+    path('news/scrapp_habr>', views.scrapp_habr_news(), name='scrapp_habr'),
 ]
